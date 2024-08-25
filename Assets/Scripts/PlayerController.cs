292425b806057a4d6cef22929ff2735963049ed3
Abhishek emmanual <abhiemm@gmail.com>
2024-08-25 21:17:00 +0530

    canimation

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+  public Rigidbody2D theRB;
+  
+  public float moveSpeed;
+
+  public Animator myAnim;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        theRB.velocity = new Vector2(Input.GetAxisRaw("Horizontal"),Input.GetAxisRaw("Vertical")) * moveSpeed;
+        
+        myAnim.SetFloat("moveX",theRB.velocity.x);
+        myAnim.SetFloat("moveY",theRB.velocity.y);
+    }
+}
