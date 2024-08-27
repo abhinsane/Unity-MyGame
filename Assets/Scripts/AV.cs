48191c4ac63a3dd870bb7cb56945f9e3eeabdcf2
Abhishek emmanual <abhiemm@gmail.com>
2024-08-27 16:08:30 +0530

    codeC

--- Assets/Scripts/AV.cs
+++ Assets/Scripts/AV.cs
@@ -2,14 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-public class AV : MonoBehaviour
+public class ARV : MonoBehaviour
 {
 
     public string areaToLoad;
+    public string areaTransitionName;
+
+    public EVE theEntrance;
 
     // Start is called before the first frame update
     void Start()
     {
+        theEntrance.transitionName = areaTransitionName;
         
     }
 
@@ -19,11 +23,13 @@ public class AV : MonoBehaviour
         
     }
 
-    private void onTriggerEnter2D(Collider2D other)
+    private void OnTriggerEnter2D(Collider2D other)
     {
         if(other.tag == "Player")
         {
             SceneManager.LoadScene(areaToLoad);
+
+            PlayerController.instance.areaTransitionName = areaTransitionName;
 
         }
     }
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,5 +1,7 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
@@ -10,10 +12,21 @@ public class PlayerController : MonoBehaviour
 
   public Animator myAnim;
 
+  public static PlayerController instance;
+  public string areaTransitionName;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+      if(instance == null)
+      {
+        instance = this;
+      }
+      else
+      {
+        Destroy(gameObject);
+      }
+      DontDestroyOnLoad(gameObject);
     }
 
     // Update is called once per frame
