4b872e8029a7022466fad5d9855d8ac2ffcce34d
Abhishek emmanual <abhiemm@gmail.com>
2024-08-29 17:32:21 +0530

    changes2

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -10,21 +10,30 @@ public class CameraController : MonoBehaviour
     public Tilemap theMap;
     private Vector3 bottomLeftLimit;
     private Vector3 topRightLimit;
+
+    private float halfhHeight;
+    private float halfWidth;
+
     // Start is called before the first frame update
     void Start()
     {
         target = PlayerController.instance.transform;
-        bottomLeftLimit = theMap.localBounds.min;
-        topRightLimit = theMap.localBounds.max;
+        
+        halfhHeight = Camera.main.orthographicSize;
+        halfWidth = halfhHeight * Camera.main.aspect; 
+
+        bottomLeftLimit = theMap.localBounds.min + new Vector3(halfWidth,halfhHeight,0f);
+        topRightLimit = theMap.localBounds.max + new Vector3(-halfWidth,-halfhHeight,0f);
+
+        PlayerController.instance.Setbounds(theMap.localBounds.min, theMap.localBounds.max);
     }
 
-    // LateUpdate is called once per frame after update
+    // Late Update is called once per frame after update
     void LateUpdate()
     {
         transform.position = new Vector3(target.position.x,target.position.y,transform.position.z);
 
         //keep the camera inside the bounds
-        transform.position = new Vector3(Mathf.Clamp(transform.position.x,bottomLeftLimit.x,topRightLimit.x), Mathf.Clamp(transform.position.y, bottomLeftLimit.y, topRightLimit.y), transform.position.z);
-
+        transform.position = new Vector3(Mathf.Clamp(transform.position.x, bottomLeftLimit.x, topRightLimit.x),Mathf.Clamp(transform.position.y, bottomLeftLimit.y, topRightLimit.y), transform.position.z);
     }
 }
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -15,6 +15,9 @@ public class PlayerController : MonoBehaviour
   public static PlayerController instance;
   public string areaTransitionName;
 
+  private Vector3 bottomLeftLimit;
+  private Vector3 topRightLimit;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -43,5 +46,15 @@ public class PlayerController : MonoBehaviour
           myAnim.SetFloat("lastMoveY",Input.GetAxisRaw("Vertical"));
           
         }
+        
+        transform.position = new Vector3(Mathf.Clamp(transform.position.x, bottomLeftLimit.x, topRightLimit.x),Mathf.Clamp(transform.position.y, bottomLeftLimit.y, topRightLimit.y), transform.position.z);
+
     }
+
+  public void Setbounds(Vector3 botLeft, Vector3 topRight)
+  {
+    bottomLeftLimit = botLeft + new Vector3(.5f,1f,0f);
+    topRightLimit = topRight + new Vector3(-.5f,-1f,0f);
+
+  }
 }
